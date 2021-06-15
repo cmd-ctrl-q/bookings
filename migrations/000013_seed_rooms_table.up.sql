5f806daa28c6ed956cde248d113a1b9fac6a8b8e
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-06-15 17:39:44 -0500

    make migration for seed rooms table

---
+++ migrations/000013_seed_rooms_table.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quarters','2020-11-18 00:00:00.000','2020-11-18 00:00:00.000'),
+	 ('Major''s Suite','2021-06-08 12:45:44.315','2021-06-08 12:45:44.315');
