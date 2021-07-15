26875936dc63c994f55b30a17a7f47f18025e0cb
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-07-15 00:17:27 -0500

    create middleware auth function that checks if user is logged in on each request

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/cmd-ctrl-q/bookings/internal/helpers"
 	"github.com/justinas/nosurf"
 )
 
@@ -33,4 +34,15 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionLoad loads and saves the sessions on every request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			session.Put(r.Context(), "error", "Log in first!")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
