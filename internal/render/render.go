60e0f8fb842193a6fbec8db92d1788251f161bfb
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-07-21 15:16:06 -0500

    create helper function for displaying human time and allow client side to use function

--- internal/render/render.go
+++ internal/render/render.go
@@ -7,6 +7,7 @@ import (
 	"html/template"
 	"net/http"
 	"path/filepath"
+	"time"
 
 	"github.com/justinas/nosurf"
 
@@ -14,7 +15,10 @@ import (
 	"github.com/cmd-ctrl-q/bookings/internal/models"
 )
 
-var functions = template.FuncMap{}
+// Specify functions that need to be accessed in templates.
+var functions = template.FuncMap{
+	"humanDate": HumanDate,
+}
 
 var app *config.AppConfig
 var pathToTemplates = "./templates"
@@ -22,6 +26,11 @@ var pathToTemplates = "./templates"
 // NewRenderer sets the config for the template package
 func NewRenderer(a *config.AppConfig) {
 	app = a
+}
+
+// HumanDate returns time in YYYY-MM-DD format
+func HumanDate(t time.Time) string {
+	return t.Format("2006-01-02")
 }
 
 func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
